5c6f89db0934b33e7123b4b344d93958b996acfa
Vitaly <86892288+Vitaliy1991777@users.noreply.github.com>
2025-05-15 03:07:25 +0500

    Update redis_test.php

--- redis_test.php
+++ redis_test.php
@@ -3,11 +3,9 @@ $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 $redis->auth('mypassword');
 
-// Простая запись и чтение
 $redis->set("key1", "Привет, Redis!");
 echo "key1: " . $redis->get("key1") . "\n";
 
-// Разные типы данных
 $redis->set("int", 123);
 $redis->lPush("list", "a", "b", "c");
 $redis->sAdd("set", "x", "y", "z");
